fa42869c1cd7e6f8b7693d995bb7e3196b7f16af
DaniGM17 <daniagm75@gmail.com>
2023-03-09 11:49:19 -0600

    LisTile

---
+++ lib/ui/screens/home_page_temp.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  const HomePageTemp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes de Flutter'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Covers Glee'),
+            subtitle: Text('Los mejores covers infravalorados'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+          ListTile(
+            title: Text('Danna Paola'),
+            subtitle: Text('Sie7e es su mejor disco'),
+          ),
+          Divider(
+            thickness: 2.0,
+            color: Color.fromARGB(255, 252, 178, 203),
+          ),
+        ],
+      ),
+    );
+  }
+}

8e2aa7b301888684ae2a8d83796a10b75ebcdbae
Dania Garcia <daniagm75@gmail.com>
2023-03-22 11:02:06 -0600

    Direccionar a una misma ruta

--- lib/ui/screens/home_page_temp.dart
+++ lib/ui/screens/home_page_temp.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/providers/menu_provider.dart';
+import 'package:componentes/ui/screens/alerts_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/utils/icons_mapping.dart';
 
@@ -29,17 +30,18 @@ Widget _lista() {
   );*/
   return FutureBuilder(
     future: menuProvider.loadData(), //indicando lo que queremos esperar
+    initialData: const [],
     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
       //print('builder: ');
       //print(snapshot.data);
       return ListView(
-        children: _listaItems(snapshot.data as List<dynamic>),
+        children: _listaItems(snapshot.data as List<dynamic>, context),
       );
     },
   );
 }
 
-List<Widget> _listaItems(List<dynamic> data) {
+List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
   final List<Widget> opciones = [];
   IconMapping iconMap = const IconMapping();
 
@@ -51,7 +53,14 @@ List<Widget> _listaItems(List<dynamic> data) {
         Icons.keyboard_arrow_right,
         color: Colors.pink,
       ),
-      onTap: () {},
+      subtitle: Text(opt['texto2']),
+      onTap: () {
+        final route = MaterialPageRoute(builder: (context) {
+          return const AlertsScreen();
+        });
+
+        Navigator.push(context, route);
+      },
     );
     opciones
       ..add(tile)
